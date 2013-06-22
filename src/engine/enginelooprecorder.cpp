2a2cc122568006add4b7af7b15d5b3103e527baf
Carl Pillot <carl.pillot@gmail.com>
2013-06-22 17:58:50 -0400

    added initial classes for loop recording. Fixed a few typos.

---
+++ src/engine/enginelooprecorder.cpp
@@ -0,0 +1,9 @@
+//
+//  enginelooprecorder.cpp
+//  Mixxx_xcode
+//
+//  Created by Carl Pillot on 6/22/13.
+//  Copyright (c) 2013 Carl Pillot. All rights reserved.
+//
+
+#include "enginelooprecorder.h"
---
+++ src/engine/enginelooprecorder.h
@@ -0,0 +1,14 @@
+//
+//  enginelooprecorder.h
+//  Mixxx_xcode
+//
+//  Created by Carl Pillot on 6/22/13.
+//  Copyright (c) 2013 Carl Pillot. All rights reserved.
+//
+
+#ifndef __Mixxx_xcode__enginelooprecorder__
+#define __Mixxx_xcode__enginelooprecorder__
+
+#include <iostream>
+
+#endif /* defined(__Mixxx_xcode__enginelooprecorder__) */
